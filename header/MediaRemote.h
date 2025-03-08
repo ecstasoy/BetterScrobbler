9b77a6b404aeb58663809a10cf1c7638a1726a82
Kunhua Huang <huang.kunh@northeastern.edu>
2025-03-07 23:41:14 -0500

    Add MediaRemote class definition and include necessary headers.

--- header/MediaRemote.h
+++ header/MediaRemote.h
@@ -5,4 +5,25 @@
 #ifndef BETTERSCROBBLER_MEDIAREMOTE_H
 #define BETTERSCROBBLER_MEDIAREMOTE_H
 
+#include <string>
+#include "LastFmScrobbler.h"
+
+class MediaRemote {
+public:
+    MediaRemote();
+
+    ~MediaRemote();
+
+    /**
+     * @brief Register for notifications from the media remote.
+     * This will allow us to receive notifications when the now playing item changes.
+     */
+    void registerForNowPlayingNotifications();
+
+private:
+    class Impl;
+
+    Impl *impl;
+};
+
 #endif //BETTERSCROBBLER_MEDIAREMOTE_H
