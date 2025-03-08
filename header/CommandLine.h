2c2222ae4806245215600fa8e3997d5022d6e9e5
Kunhua Huang <huang.kunh@northeastern.edu>
2025-03-08 10:51:14 -0500

    Add CommandLine class to process commandline arguments

--- header/CommandLine.h
+++ header/CommandLine.h
@@ -5,6 +5,47 @@
 #ifndef BETTERSCROBBLER_COMMANDLINE_H
 #define BETTERSCROBBLER_COMMANDLINE_H
 
+#include <string>
+#include <vector>
+#include <iostream>
 #include "Config.h"
+#include "Logger.h"
+
+class CommandLine {
+public:
+    static void parse(int argc, char *argv[]) {
+        for (int i = 1; i < argc; i++) {
+            std::string arg = argv[i];
+            processArg(arg);
+        }
+    }
+
+    static void printUsage(const char *programName) {
+        std::cout << "Usage: " << programName << " [options]\n"
+                  << "Options:\n"
+                  << "  --daemon    Run as a daemon process\n"
+                  << "  --log=PATH  Specify custom log file path\n"
+                  << "  --help      Show this help message\n";
+    }
+
+private:
+    static void processArg(const std::string &arg) {
+        if (arg == "--debug") {
+            Logger::getInstance().setDebugEnabled(true);
+        }
+        if (arg == "--daemon") {
+            Config::getInstance().setDaemonMode(true);
+        } else if (arg.substr(0, 6) == "--log=") {
+            Config::getInstance().setLogPath(arg.substr(6));
+        } else if (arg == "--help") {
+            printUsage(Config::getInstance().getAppName().c_str());
+            exit(0);
+        } else {
+            LOG_ERROR("Unknown argument: " + arg);
+            printUsage(Config::getInstance().getAppName().c_str());
+            exit(1);
+        }
+    }
+};
 
 #endif //BETTERSCROBBLER_COMMANDLINE_H
