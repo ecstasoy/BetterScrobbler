a38fd8e40fabfd99fdcd2c2a2240e752ee17b178
Kunhua Huang <huang.kunh@northeastern.edu>
2025-03-08 11:30:19 -0500

    Add UrlUtils class with helper functions to process http request and handle urls

--- header/UrlUtils.h
+++ header/UrlUtils.h
@@ -5,4 +5,20 @@
 #ifndef BETTERSCROBBLER_URLUTILS_H
 #define BETTERSCROBBLER_URLUTILS_H
 
+#include <string>
+#include <map>
+
+class UrlUtils {
+public:
+    static std::string urlEncode(const std::string &input);
+
+    static std::string buildUrl(const std::string &baseUrl,
+                                const std::map<std::string, std::string> &params);
+
+    static std::string md5(const std::string &input);
+
+    static std::string generateApiSignature(const std::map<std::string, std::string> &params,
+                                            const std::string &apiSecret);
+};
+
 #endif //BETTERSCROBBLER_URLUTILS_H
