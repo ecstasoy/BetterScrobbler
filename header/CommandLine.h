86e3ca5c9229feda8f5f5b922272c5b681028364
Kunhua Huang <huang.kunh@northeastern.edu>
2025-03-08 15:11:47 -0500

    refactor: simplify command line argument parsing and improve help message

--- header/CommandLine.h
+++ header/CommandLine.h
@@ -14,38 +14,37 @@
 class CommandLine {
 public:
     static void parse(int argc, char *argv[]) {
+        auto &config = Config::getInstance();
+        auto &logger = Logger::getInstance();
+
         for (int i = 1; i < argc; i++) {
             std::string arg = argv[i];
-            processArg(arg);
+
+            if (arg == "--daemon") {
+                config.setDaemonMode(true);
+            } else if (arg == "--debug") {
+                logger.setDebugEnabled(true);
+            } else if (arg.substr(0, 6) == "--log=") {
+                config.setLogPath(arg.substr(6));
+            } else if (arg == "--help") {
+                showHelp();
+                exit(0);
+            } else {
+                LOG_ERROR("Unknown argument: " + arg);
+                showHelp();
+                exit(1);
+            }
         }
     }
 
-    static void printUsage(const char *programName) {
-        std::cout << "Usage: " << programName << " [options]\n"
+private:
+    static void showHelp() {
+        std::cout << "Usage: Scrobbler [options]\n"
                   << "Options:\n"
                   << "  --daemon    Run as a daemon process\n"
                   << "  --debug     Show debug message in the console\n"
                   << "  --log=PATH  Specify custom log file path\n"
                   << "  --help      Show this help message\n";
-    }
-
-private:
-    static void processArg(const std::string &arg) {
-        if (arg == "--debug") {
-            Logger::getInstance().setDebugEnabled(true);
-        }
-        if (arg == "--daemon") {
-            Config::getInstance().setDaemonMode(true);
-        } else if (arg.substr(0, 6) == "--log=") {
-            Config::getInstance().setLogPath(arg.substr(6));
-        } else if (arg == "--help") {
-            printUsage(Config::getInstance().getAppName().c_str());
-            exit(0);
-        } else {
-            LOG_ERROR("Unknown argument: " + arg);
-            printUsage(Config::getInstance().getAppName().c_str());
-            exit(1);
-        }
     }
 };
 
