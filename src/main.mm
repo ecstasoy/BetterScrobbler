758f9713f7935ef0c8c82c150e1b5cd84024a497
Kunhua Huang <huang.kunh@northeastern.edu>
2025-03-08 13:50:34 -0500

    Implement main function with daemon mode support and application initialization

--- src/main.mm
+++ src/main.mm
@@ -8,3 +8,28 @@
 #import "header/Logger.h"
 #import "header/CommandLine.h"
 #import "header/Credentials.h"
+
+int main(int argc, char *argv[]) {
+    @autoreleasepool {
+        CommandLine::parse(argc, argv);
+        Logger::getInstance().init(Config::getInstance().isDaemonMode());
+
+        if (Config::getInstance().isDaemonMode()) {
+            if (daemon(0, 0) == -1) {
+                LOG_ERROR("Failed to daemonize process: " + std::string(strerror(errno)));
+                return 1;
+            }
+            LOG_INFO("Starting scrobbler in daemon mode...");
+        }
+
+        if (!Credentials::getInstance().checkAndPrompt()) {
+            return 1;
+        }
+
+        MediaRemote bridge;
+        bridge.registerForNowPlayingNotifications();
+
+        LOG_INFO("Scrobbler is running...");
+        CFRunLoopRun();
+    }
+}
