71dda81d204ac0446482778b510e28db8737f961
Kunhua Huang <huang.kunh@northeastern.edu>
2025-03-07 23:55:11 -0500

    Add helper function to retrieve current track duration from Apple Music application via AppleScript.

--- src/Helper.mm
+++ src/Helper.mm
@@ -3,3 +3,24 @@
 //
 
 #include "header/Config.h"
+#include "header/Logger.h"
+#include "header/Helper.h"
+
+double getAppleMusicDuration() {
+    FILE *pipe = popen("osascript -e 'tell application \"Music\" to get duration of current track'", "r");
+    if (!pipe) {
+        return 0.0;
+    }
+    char buffer[128];
+    std::string result;
+    while (fgets(buffer, sizeof(buffer), pipe) != NULL) {
+        result += buffer;
+    }
+    pclose(pipe);
+
+    try {
+        return std::stod(result);
+    } catch (...) {
+        return 0.0;
+    }
+}
