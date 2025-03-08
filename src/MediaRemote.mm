8c2fe2e5982c893ecdedbde123fcc58c005d238b
Kunhua Huang <huang.kunh@northeastern.edu>
2025-03-07 23:44:01 -0500

    Initialize MediaRemote implementation with CoreFoundation dependencies and private implementation class structure.

--- src/MediaRemote.mm
+++ src/MediaRemote.mm
@@ -1,3 +1,27 @@
 //
 // Created by Kunhua Huang on 3/7/25.
 //
+
+#include <CoreFoundation/CoreFoundation.h>
+#inclide <dispatch/dispatch.h>
+#include <header/MediaRemote.h>
+
+typedef void (*MRMediaRemoteGetNowPlayingInfo_t)(dispatch_queue_t, void(^)(CFDictionaryRef));
+
+class MediaRemote::Impl {
+
+};
+
+#pragma mark - MediaRemoteBridge
+
+MediaRemote::MediaRemote()
+        : impl(new Impl) {
+}
+
+MediaRemote::~MediaRemote() {
+    delete impl;
+}
+
+void MediaRemote::registerForNowPlayingNotifications() {
+
+}
